46e39841b63ed335b0f5742052a7684d3d2b73fa
Mayur Dushetwar <114808463+mdushetwar@users.noreply.github.com>
2023-04-23 11:44:42 +0530

    version number changed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.1.3'
+VERSION = '0.1.4'
 DESCRIPTION = 'MGD_Outliers is a Pypyththon package for identifying and visualizing outliers in a DataFrame.'
 
 
